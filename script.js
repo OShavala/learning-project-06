9389532224e1ba0d6fa05a67aea408c52c4f654c
OShavala <olena.shvl@gmail.com>
2024-01-31 09:25:14 +0200

    second fixation

---
+++ script.js
@@ -0,0 +1,23 @@
+function shareOnFacebook() {
+    let facebookUrl = 'https://facebook.com/';
+    window.open(facebookUrl, '_blank');
+}
+
+
+function shareOnTwitter() {
+    let twitterUrl = 'https://twitter.com/';
+    window.open( twitterUrl, '_blank');
+}
+
+function shareOnLinkedIn() {
+  let linkedInUrl = 'https://ua.linkedin.com/';
+  window.open( linkedInUrl, '_blank');
+}
+
+function shareOnInstagram() {
+   let instagramUrl = 'https://instagram.com/';
+   window.open( instagramUrl, '_blank');
+}
+
+
+
